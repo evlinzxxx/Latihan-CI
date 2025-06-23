40012eea66e88883226bae0644a636ef4cac8075
Evlin Sitanggang <105533381+evlinzxxx@users.noreply.github.com>
2025-06-23 13:53:50 +0700

    Update modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -7,14 +7,16 @@ import numpy as np
 import os
 import warnings
 import sys
- 
+
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
- 
+
+    # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
     file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "train_pca.csv")
     data = pd.read_csv(file_path)
- 
+
+    # The predicted column is "quality" which is a scalar from [3, 9]
     X_train, X_test, y_train, y_test = train_test_split(
     data.drop("Credit_Score", axis=1),
     data["Credit_Score"],
@@ -24,20 +26,18 @@ if __name__ == "__main__":
     input_example = X_train[0:5]
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
- 
     with mlflow.start_run():
-        # Train model
         model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
         model.fit(X_train, y_train)
 
         predicted_qualities = model.predict(X_test)
-        
-        accuracy = model.score(X_test, y_test)
-        mlflow.log_metric("accuracy", accuracy)
-
 
         mlflow.sklearn.log_model(
-            sk_model=model,
-            artifact_path="model",
-            input_example=input_example
+        sk_model=model,
+        artifact_path="model",
+        input_example=input_example
         )
+        model.fit(X_train, y_train)
+        # Log metrics
+        accuracy = model.score(X_test, y_test)
+        mlflow.log_metric("accuracy", accuracy)
